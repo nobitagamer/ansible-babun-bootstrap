b88645629794c19e23c64d9f7644df99d5a61868
Kevin Edwards <kedwards@kncedwards.com>
2017-03-23 14:01:48 -0600

    Fixes for cli install.

--- ansible-babun-bootstrap.sh
+++ ansible-babun-bootstrap.sh
@@ -4,7 +4,6 @@ HOME=~
 ANSIBLE_DIR=$HOME/ansible
 CURRENT_DIR=$(pwd)
 AWS_CLI=0
-DEPS="pywinrm cryptography"
 
 if [ -f /etc/ansible-babun-bootstrap.completed ]
 then
@@ -26,28 +25,46 @@ then
 	sleep 2
 	clear
 else
-	# Ansible dependencies
-	pact install curl figlet gcc-g++ git libyaml-devel openssh openssl opensssl-devel python python-crypto python-devel python-jinja2 python-paramiko python-setuptools python-yaml vim wget &> /dev/null
+	printf "~ MRM Ansible Install ~\n\n"
+	printf "installing dependencies.."
+	pact install figlet gcc-g++ libffi-devel libyaml-devel python python-crypto python-jinja2 python-paramiko python-yaml libyaml-devel python-setuptools python-pip python-devel &> /dev/null
 	easy_install-2.7 pip &> /dev/null
 	
-	# AWS CLI
 	if [ $AWS_CLI = 1 ] 
 	then
-		DEPS="${DEPS} httplib2 boto awscli"
+		pip install pywinrm cryptography httplib2 boto awscli &> /dev/null
+	else
+		pip install pywinrm cryptography &> /dev/null
 	fi
+	printf ".ok\n"
 	
-	pip install ${DEPS}
+	printf "installing ansible.."
+	mkdir -p $ANSIBLE_DIR
+	git clone https://github.com/ansible/ansible.git --recursive $ANSIBLE_DIR  &> /dev/null
+	cd $ANSIBLE_DIR
+	source ./hacking/env-setup &> /dev/null
+	cd $CURRENT_DIR
+	
+	cp $ANSIBLE_DIR/examples/ansible.cfg ~/.ansible.cfg
+	# Use paramiko to allow passwords
+	sed -i 's|#\?transport.*$|transport = paramiko|' ~/.ansible.cfg
+	# Disable host key checking for performance
+	sed -i 's|#host_key_checking = False|host_key_checking = False|' ~/.ansible.cfg
+	# touch a file to mark first app init completed
+	touch /etc/ansible-babun-bootstrap.completed
+    printf ".ok\n"    
 	
 	# Create initial Ansible hosts inventory and test workspace
-	mkdir -p $HOME/ansible_workspace/test/{conf,inventory} /etc/ansible
-	touch $HOME/ansible_workspace/test/conf/{.ansible_vault2,vault_key2}
-	cat > tee $HOME/ansible_worksapce/test/inventory/hosts /etc/ansible/hosts << 'EOF'
+	printf "creating test project.."
+	mkdir -p ~/ansible_workspace/test/{conf,inventory}
+	touch ~/ansible_workspace/test/conf/{.ansible_vault2,vault_key2}
+	cat > ~/ansible_workspace/test/inventory/hosts << 'EOF'
 # Local control machine
 [local]
 localhost ansible_connection=local
 EOF
     
-	cat > $HOME/ansible_worksapce/test/ansible.cfg << 'EOF'
+	cat > ~/ansible_workspace/test/ansible.cfg << 'EOF'
 [defaults]
 ansible_managed = Ansible managed: {file} modified on %Y-%m-%d %H:%M:%S by {uid} on {host}
 inventory = inventory/
@@ -73,28 +90,11 @@ control_path = /tmp/ansible-ssh-%%h-%%p-%%r
 [privilege_escalation]
 become_user = true	
 EOF
+	printf ".ok\n"    
 	
-	# Setup Ansible from Source
-	mkdir -p $ANSIBLE_DIR
-	git clone https://github.com/ansible/ansible.git --recursive $ANSIBLE_DIR &> /dev/null
-	cd $ANSIBLE_DIR
-	source ./hacking/env-setup &> /dev/null
-	cd $CURRENT_DIR
-
-	# Copy default config
-	cp $ANSIBLE_DIR/examples/ansible.cfg ~/.ansible.cfg
-
-	# Use paramiko to allow passwords
-	sed -i 's|#\?transport.*$|transport = paramiko|' ~/.ansible.cfg
-
-	# Disable host key checking for performance
-	sed -i 's|#host_key_checking = False|host_key_checking = False|' ~/.ansible.cfg
-	
-	# touch a file to mark first app init completed
-	touch /etc/ansible-babun-bootstrap.completed
-	
+	printf "configuring zshell for ansible.."
 	# Set this script to run at Babun startup
-	cat >> $HOME/.zshrc <<'EOF'
+	cat >> ~/.zshrc <<'EOF'
 
 #
 # Ansible in Babun
@@ -104,12 +104,12 @@ EOF
 export BOOTSTRAP_ANSIBLE_UPDATE=0
 
 # Configure Babun for Ansible
-source $HOME/ansible-babun-bootstrap/ansible-babun-bootstrap.sh
+source ~/ansible-babun-bootstrap/ansible-babun-bootstrap.sh
 
 # Figlet Banner
 figlet "MRM Automation"
-cd $HOME/ansible_workspace
+cd ~/ansible_workspace
 EOF
-
+	printf ".ok\n\n"    
 	echo "Ansible in Babun completed, please restart Babun!"
 fi
