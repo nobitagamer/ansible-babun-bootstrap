2da79e270f9e815e0299bc767807fb9d2d6fd112
Kevin Edwards <kedwards@kncedwards.com>
2017-04-17 14:45:59 -0600

    fixed cd into giit project before branch checkout.

--- src/install.sh
+++ src/install.sh
@@ -3,6 +3,7 @@
 if [ ! -d ~/ansible-babun-bootstrap ]
 then
   git clone https://github.com/kedwards/ansible-babun-bootstrap.git ~/ansible-babun-bootstrap
+  cd ~/ansible-babun-bootstrap
   git checkout mrm
 else
   cd ~/ansible-babun-bootstrap
